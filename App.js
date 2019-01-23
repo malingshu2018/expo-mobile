7fa64c511925fd3419620c988b33d69b65386305
malingshu <malingshu@gmail.com>
2019-01-23 15:23:25 +0700

    添加文件

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Platform, StatusBar } from 'react-native';
+
+import {AppLoading, Asset, Font, Icon} from 'expo'
+import AppNavigator from "./navigation/AppNavigator";
+
 
 export default class App extends React.Component {
+  state = {
+      isLoadingComplete: false
+  };
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    if(!this.state.isLoadingComplete && !this.props.skipLoadingScreen){
+        return (
+          <AppLoading
+            startAsync = {this._loadResourcesAsync}
+            onError={this._handleLoadingError}
+            onFinish={this._handleFinishLoading}
+          />
+        );
+    }else{
+        return (
+          <View style={styles.container}>
+              {Platform.OS === 'ios' && <StatusBar barStyle='default'/>}
+              <AppNavigator/>
+          </View>
+        );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all(
+        Asset.loadAsync([
+            require('./assets/images/robot-dev.png'),
+            require('./assets/images/robot-prod.png')
+        ]),
+        Font.loadAsync({
+            ...Icon.Ionicons.font,
+            'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+        })
+    )
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error)
+  };
+
+  _handleFinishLoading =() => {
+    this.setState({isLoadingComplete: true})
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
